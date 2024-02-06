534af6496ef958432fa8bdd480159445400c451f
Keyan <keyanjavanshir@live.dk>
2024-02-06 16:35:05 +0100

    Imported public folder containing all UI elements.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image 
+          src="/hilink-logo.svg" alt="logo" width={74} height={29}/> {/* Using nextjs Image component. It is an optimized version of images that can significantly reduce load times. */} 
+      </Link>
+    </nav>
   )
 }
 
