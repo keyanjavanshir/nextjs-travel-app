2838df37f8a68a095f07b2e9dd96a02f14f3881d
Keyan <keyanjavanshir@live.dk>
2024-02-06 16:16:10 +0100

    Created component folder: added Navbar and Footer. Imported navbar and footer into layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <h1 className="bold-88 regular-40 text-teal-500">
+    <h1 className="font-bold text-teal-500 text-2xl">
       Hello world!
     </h1>
   );
