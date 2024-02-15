a173ee869755793a7602112363bd37f232f3c0ae
Keyan <keyanjavanshir@live.dk>
2024-02-15 13:28:17 +0100

    Fixed hero-map positioning. Added a medium device class for description.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Button from "./Button"
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-60 xl:flex-row">
       <div className="hero-map" />
 
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
@@ -15,7 +15,7 @@ const Hero = () => {
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
         <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
-        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px] md:max-w-[580px]">
           We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
         </p>
 
