7593e524777cf499e843be8e6ec0a342693a339a
Keyan <keyanjavanshir@live.dk>
2024-02-24 16:48:19 +0100

    missed comments

--- components/Features.tsx
+++ components/Features.tsx
@@ -67,7 +67,7 @@ const FeatureItem = ({ title, icon, description }: FeatureItem) => {
       <h2 className="bold-20 lg:bold-32 mt-5 capitalize">
         {title}
       </h2>
-      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] md:bg-none">
+      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
         {description}
       </p>
     </li>
