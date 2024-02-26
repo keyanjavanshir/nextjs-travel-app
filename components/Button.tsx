c83484946212f66bd5c02d56741fc63be8e5ea4d
Keyan <keyanjavanshir@live.dk>
2024-02-26 19:37:40 +0100

    Finished GetApp section

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,14 +6,16 @@ type ButtonProps = {
     title: string;
     icon?: string;
     variant: string;
+    // By giving it the ? attribute, we make it an optional attribute instead of required.
+    full?: boolean;
 }
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
-    <button className={`flexCenter gap-3 rounded-full border ${variant}`}
+    <button className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
     type={type}>
         {icon && <Image src={icon} alt={title} width={24} height={24} />}
-        <label className="bold-16 whitespace-nowrap">{title}</label>
+        <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
   )
 }
